d6c847ececcb01fa7fa01e2ded6a8b8a6884393f
soelapyae-htun <e1045754@u.nus.edu>
2022-11-07 19:26:37 +0800

    first exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React from "react";
 
-class App extends React.Component{
+
+
+class Item extends React.Component{
   render(){
-    return <h1>Hello world</h1>
+    return <li>{this.props.name}, ${this.props.price}</li>
   }
 }
 
-export default App;
+
+
+class App extends React.Component{
+
+  state = {
+    items:[
+      {id:1,name:"orange",price:1.99},
+      {id:2,name:"apple",price:2.99},
+    ]
+  }
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+
+  add = () => {
+
+    let id = this.state.items.length + 1
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+    this.setState({
+      items:[
+        ...this.state.items,
+        {id,name,price}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <ul>
+        {this.state.items.map(i => {
+          return <Item key={i.id} name={i.name} price={i.price} />
+        })}
+        </ul>
+        <input type="text" ref={this.nameRef} />
+        <input type="text" ref={this.priceRef} />
+        <button onClick={this.add}>add</button>
+      </div>
+    )
+  }
+}
+
+export default App

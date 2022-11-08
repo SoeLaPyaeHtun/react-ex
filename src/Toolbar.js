d03644acf40d05d27487a6331554c0f93869b8dd
soelapyae-htun <e1045754@u.nus.edu>
2022-11-08 10:37:15 +0800

    testing css in js

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,12 +1,25 @@
 import React from "react";
 import './Toolbar.css';
 
+const styles = {
+    toolbar: {
+        marginBottom: 20,
+        border: '1px solid red',
+    },
+    dark:{
+        background: 'purple',
+        color: 'black',
+    }
+}
 
 
 class Toolbar extends React.Component{
+
+   
+
     render(){
         return (
-            <div className="toolbar">
+            <div style={styles.toolbar, styles.dark}>
                {this.props.children}
             </div>
         )

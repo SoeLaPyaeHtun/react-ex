ee7e1cf334f68bdef4d937b68dc944cbe1144d37
soelapyae-htun <e1045754@u.nus.edu>
2022-11-08 13:39:11 +0800

    testing context

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import React, {createRef, useState} from "react";
+import React from "react";
 
-
-const Items = ({name, price}) => (
-  <li>{name}, ${price}</li>
-)
-
+const myContext = React.createContext("nothing");
 
 const App = props => {
-  let [items, setItems ] = useState([
-    { id: 1, name: 'Apple', price: 0.99 },
-    { id: 2, name: 'Orange', price: 0.89 },
-  ])
+  return <Header />
+}
 
-let nameRef = createRef();
-let priceRef = createRef();
+const Header = props => {
+  return <Title />
+}
 
-  let add = () => {
-    let id = items.length + 1
-    let name = nameRef.current.value;
-    let price = priceRef.current.value;
-    setItems([
-      ...items,
-    {id, name, price}
-    ])
-  }
-
-  return(
-    <div>
-      <h1>Hello</h1>
-      <ul>
-      {items.map(i => (<Items key={i.id} name={i.name} price={i.price}/>))}
-    
-      </ul>
-      <input type="text" ref={nameRef} /> <br/>
-      <input type="text" ref={priceRef} />
-      <button onClick={add}>add</button>
-    </div>
-
-  )
+const Title = props => {
+  const name = React.useContext(myContext);
+  return <h1>{name}</h1>
 }
 
 export default App
-

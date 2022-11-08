06a3d376bc7eba8ab9194a90d1e2a8d95b4dfbc1
soelapyae-htun <e1045754@u.nus.edu>
2022-11-08 10:26:19 +0800

    using seprate files

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
 import React from "react";
-
-
-
-class Item extends React.Component{
-  render(){
-    return <li>{this.props.name}, ${this.props.price}</li>
-  }
-}
+import Toolbar from "./Toolbar";
 
 
 
 class App extends React.Component{
-
-  state = {
-    items:[
-      {id:1,name:"orange",price:1.99},
-      {id:2,name:"apple",price:2.99},
-    ]
-  }
-
-  nameRef = React.createRef();
-  priceRef = React.createRef();
-
-  add = () => {
-
-    let id = this.state.items.length + 1
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
-    this.setState({
-      items:[
-        ...this.state.items,
-        {id,name,price}
-      ]
-    })
-  }
-
-  render() {
+  render(){
     return (
       <div>
-        <h1>Hello world</h1>
-        <ul>
-        {this.state.items.map(i => {
-          return <Item key={i.id} name={i.name} price={i.price} />
-        })}
-        </ul>
-        <input type="text" ref={this.nameRef} />
-        <input type="text" ref={this.priceRef} />
-        <button onClick={this.add}>add</button>
+        <Toolbar>
+          <h1>Hello</h1>
+          <p>This is simple paragraph</p>
+        </Toolbar>
       </div>
     )
   }
 }
 
-export default App
+export default App;
